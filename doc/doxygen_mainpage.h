d9c881d2ca8d9cf9db61e267edcc3163d244d7eb
Frank Mori Hess <fmhess@speakeasy.net>
2007-03-10 01:45:21 +0000

    Filled out main page of doxygen docs

--- doc/doxygen_mainpage.h
+++ doc/doxygen_mainpage.h
@@ -1,6 +1,29 @@
 /* This header exists purely for the sake of doxygen. */
 
 /*! \mainpage libpoet Documentation
-\section my section
-this is my documentation.
+\section description Parallel Object Execution Threads
+libpoet is a framework intended to make creation of active objects easy
+enough for routine use.  Some of the more important classes in the library
+are poet::future, poet::active_function, and poet::scheduler.  To learn
+more about active objects, see the paper
+<a href="http://www.cse.wustl.edu/~doc/pspdfs/Act-Obj.pdf">"Active Object, An Object Behavioral Pattern for Concurrent Programming." by
+R. Greg Lavender and Douglas C. Schmidt</a>.
+
+\section download Download
+
+The current source code is availabe via anonymous cvs:
+\code
+cvs -d :pserver:anonymous@cvs.comedi.org:/cvs/comedi login
+cvs -d :pserver:anonymous@cvs.comedi.org:/cvs/comedi co libpoet
+\endcode
+
+When prompted for a password, hit enter.
+
+You can also
+<a href="http://www.comedi.org/cgi-bin/viewcvs.cgi/libpoet/">browse the cvs online</a>.
+
+The code is licensed under the <a href="http://www.boost.org/LICENSE_1_0.txt">Boost Software License, Version 1.0</a>.
+
+\author Frank Mori Hess <fmhess@users.sourceforge.net>
+
 */
