98d4af0c7be19375270b55f00545cc05cd8c199e
Frank Mori Hess <fmhess@speakeasy.net>
2007-03-20 13:25:57 +0000

    Added test of exception forwarding.

---
+++ test/exception_test.cpp
@@ -0,0 +1,142 @@
+
+#include <boost/assert.hpp>
+#include <iostream>
+#include <poet/active_function.hpp>
+#include <stdexcept>
+
+void throw_runtime_error()
+{
+	std::cerr << __FUNCTION__ << std::endl;
+	throw std::runtime_error("this std::runtime_error exception was transported to a future.");
+}
+
+int throw_logic_error()
+{
+	std::cerr << __FUNCTION__ << std::endl;
+	throw std::logic_error("this std::logic_error exception was transported to a future.");
+	return 0;
+}
+
+void throw_out_of_range()
+{
+	std::cerr << __FUNCTION__ << std::endl;
+	throw std::out_of_range("this std::out_of_range exception was transported to a future.");
+}
+
+class custom_exception: public std::exception
+{
+public:
+	custom_exception()
+	{}
+	virtual ~custom_exception() throw() {}
+	virtual const char * what() const throw() {return "custom_exception";}
+};
+
+void throw_unknown_exception()
+{
+	std::cerr << __FUNCTION__ << std::endl;
+	throw custom_exception();
+}
+
+struct unknown
+{
+	int value;
+};
+
+void throw_really_unknown()
+{
+	std::cerr << __FUNCTION__ << std::endl;
+	throw unknown();
+}
+
+int main()
+{
+	{
+		poet::active_function<void ()> thrower(&throw_runtime_error);
+		poet::future<void> result = thrower();
+		try
+		{
+			static_cast<void>(result);
+		}
+		catch(const std::runtime_error &err)
+		{
+			std::cerr << "caught exception: " << err.what() << std::endl;
+		}
+		catch(...)
+		{
+			BOOST_ASSERT(false);
+		}
+	}
+
+	{
+		poet::active_function<int ()> thrower(&throw_logic_error);
+		poet::future<int> result = thrower();
+		try
+		{
+			int retval = result;
+			std::cout << "retval is " << retval << std::endl;
+		}
+		catch(const std::logic_error &err)
+		{
+			std::cerr << "caught exception: " << err.what() << std::endl;
+		}
+		catch(...)
+		{
+			BOOST_ASSERT(false);
+		}
+	}
+
+	{
+		poet::active_function<void ()> thrower(&throw_out_of_range);
+		poet::future<void> result = thrower();
+		try
+		{
+			static_cast<void>(result);
+		}
+		catch(const std::out_of_range &err)
+		{
+			std::cerr << "caught exception: " << err.what() << std::endl;
+		}
+		catch(...)
+		{
+			BOOST_ASSERT(false);
+		}
+	}
+
+	// throw an exception derived from std::exception
+	{
+		poet::active_function<void ()> thrower(&throw_unknown_exception);
+		poet::future<void> result = thrower();
+		try
+		{
+			static_cast<void>(result);
+		}
+		catch(const poet::unknown_exception &err)
+		{
+			std::cerr << "caught exception: " << err.what() << std::endl;
+		}
+		catch(...)
+		{
+			BOOST_ASSERT(false);
+		}
+	}
+
+	// throw completely unknown exception
+	{
+		poet::active_function<void ()> thrower(&throw_really_unknown);
+		poet::future<void> result = thrower();
+		try
+		{
+			static_cast<void>(result);
+		}
+		catch(const poet::unknown_exception &err)
+		{
+			std::cerr << "caught exception: " << err.what() << std::endl;
+		}
+		catch(...)
+		{
+			BOOST_ASSERT(false);
+		}
+	}
+	return 0;
+}
