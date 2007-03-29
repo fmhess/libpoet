2b85382d4136db4f67d0e35991441d4f5eef7461
Frank Mori Hess <fmhess@speakeasy.net>
2007-03-29 14:33:12 +0000

    Added explicit use of promise class to pipline example program.

--- examples/pipeline.cpp
+++ examples/pipeline.cpp
@@ -33,16 +33,13 @@ double square_root(double a)
 int main()
 {
 	static const unsigned num_vectors = 10;
-
-	// input data, assigned some arbitrary values
-	std::vector<boost::array<double, 2> > input_vectors;
 	unsigned i;
+
+	// input data, assigned actual values later
+	std::vector<boost::array<poet::promise<double>, 2> > promises;
 	for(i = 0; i < num_vectors; ++i)
 	{
-		boost::array<double, 2> vec_2d;
-		vec_2d.at(0) = i % 4;
-		vec_2d.at(1) = i % 3;
-		input_vectors.push_back(vec_2d);
+		promises.push_back(boost::array<poet::promise<double>, 2>());
 	}
 
 	// active_functions
@@ -52,11 +49,11 @@ int main()
 	poet::active_function<double (double)> active_sqrt(&square_root);
 
 	std::vector<poet::future<double> > lengths;
-	// pass input data through active_function pipeline
-	for(i = 0; i < input_vectors.size(); ++i)
+	// pass promises through our active_function pipeline
+	for(i = 0; i < promises.size(); ++i)
 	{
-		poet::future<double> product0 = active_multiplier0(input_vectors.at(i).at(0), input_vectors.at(i).at(0));
-		poet::future<double> product1 = active_multiplier1(input_vectors.at(i).at(1), input_vectors.at(i).at(1));
+		poet::future<double> product0 = active_multiplier0(promises.at(i).at(0), promises.at(i).at(0));
+		poet::future<double> product1 = active_multiplier1(promises.at(i).at(1), promises.at(i).at(1));
 
 		poet::future<double> sum = active_adder(product0, product1);
 
@@ -65,12 +62,22 @@ int main()
 		lengths.push_back(root);
 	}
 
+	// fulfill input promises
+	for(i = 0; i < promises.size(); ++i)
+	{
+		promises.at(i).at(0).fulfill(i % 4);
+		promises.at(i).at(1).fulfill(i % 3);
+	}
+
 	// wait for futures to become ready and convert them to values
 	for(i = 0; i < lengths.size(); ++i)
 	{
+		boost::array<poet::future<double>, 2> vec;
+		vec.at(0) = promises.at(i).at(0);
+		vec.at(1) = promises.at(i).at(1);
 		double value = lengths.at(i);
-		std::cout << "vector " << i << " = {" << input_vectors.at(i).at(0) << ", " <<
-			input_vectors.at(i).at(1) << "}, length = " << value << "\n";
+		std::cout << "vector " << i << " = {" << vec.at(0) << ", " <<
+			vec.at(1) << "}, length = " << value << "\n";
 	}
 
 	return 0;
