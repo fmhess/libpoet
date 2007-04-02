28b76b246abd40a6cce07f15128f6f6439b729f3
Frank Mori Hess <fmhess@speakeasy.net>
2007-04-02 19:59:27 +0000

    Moved active_function_method_request back into poet::detail namespace, as it isn't very useful by itself after all.

--- poet/detail/active_function_template.hpp
+++ poet/detail/active_function_template.hpp
@@ -46,142 +46,150 @@
 
 namespace poet
 {
-	template <typename Signature>
-	class POET_AF_METHOD_REQUEST_CLASS_NAME:
-		public method_request<typename  boost::function_traits<Signature>::result_type>
+	namespace detail
 	{
-	public:
-		typedef typename boost::function_traits<Signature>::result_type passive_result_type;
-		typedef method_request<passive_result_type> base_type;
-		typedef typename boost::slot<Signature> passive_slot_type;
-		typedef typename boost::slot<bool ()> guard_slot_type;
+		template <typename Signature>
+		class POET_AF_METHOD_REQUEST_CLASS_NAME:
+			public method_request<typename  boost::function_traits<Signature>::result_type>
+		{
+		public:
+			typedef typename boost::function_traits<Signature>::result_type passive_result_type;
+			typedef method_request<passive_result_type> base_type;
+			typedef typename boost::slot<Signature> passive_slot_type;
+			typedef typename boost::slot<bool ()> guard_slot_type;
 
-		// static factory method
-		static boost::shared_ptr<POET_AF_METHOD_REQUEST_CLASS_NAME<Signature> > create(
-			promise<passive_result_type> returnValue,
-			POET_ACTIVE_FUNCTION_FULL_ARGS(POET_ACTIVE_FUNCTION_NUM_ARGS, Signature) BOOST_PP_COMMA_IF(POET_ACTIVE_FUNCTION_NUM_ARGS)
-			const boost::shared_ptr<passive_slot_type> &passive_function,
-			const boost::shared_ptr<guard_slot_type> &guard = boost::shared_ptr<guard_slot_type>())
-		{
-			return deconstruct_ptr(new POET_AF_METHOD_REQUEST_CLASS_NAME<Signature>(
-				returnValue, POET_ACTIVE_FUNCTION_ARG_NAMES(POET_ACTIVE_FUNCTION_NUM_ARGS, arg)
-				BOOST_PP_COMMA_IF(POET_ACTIVE_FUNCTION_NUM_ARGS) passive_function, guard));
-		}
-		virtual ~POET_AF_METHOD_REQUEST_CLASS_NAME()
-		{
-		}
-		virtual void run()
-		{
-			try
+			// static factory method
+			static boost::shared_ptr<POET_AF_METHOD_REQUEST_CLASS_NAME<Signature> > create(
+				promise<passive_result_type> returnValue,
+				POET_ACTIVE_FUNCTION_FULL_ARGS(POET_ACTIVE_FUNCTION_NUM_ARGS, Signature) BOOST_PP_COMMA_IF(POET_ACTIVE_FUNCTION_NUM_ARGS)
+				const boost::shared_ptr<passive_slot_type> &passive_function,
+				const boost::shared_ptr<guard_slot_type> &guard = boost::shared_ptr<guard_slot_type>())
 			{
-				passive_result_type *resolver = 0;
-				m_run(resolver);
+				return deconstruct_ptr(new POET_AF_METHOD_REQUEST_CLASS_NAME<Signature>(
+					returnValue, POET_ACTIVE_FUNCTION_ARG_NAMES(POET_ACTIVE_FUNCTION_NUM_ARGS, arg)
+					BOOST_PP_COMMA_IF(POET_ACTIVE_FUNCTION_NUM_ARGS) passive_function, guard));
 			}
-			catch(...)
+			virtual ~POET_AF_METHOD_REQUEST_CLASS_NAME()
 			{
-				this->_returnValue.renege(current_exception());
 			}
-		}
-		virtual bool ready() const
-		{
-			/* We return ready if any of the future inputs has an exception. */
-			// if(_argn.has_exception() == true) return true;
+			virtual void run()
+			{
+				try
+				{
+					passive_result_type *resolver = 0;
+					m_run(resolver);
+				}
+				catch(...)
+				{
+					this->_returnValue.renege(current_exception());
+				}
+			}
+			virtual bool ready() const
+			{
+				/* We return ready if any of the future inputs has an exception. */
+				// if(_argn.has_exception() == true) return true;
 #define POET_ACTIVE_FUNCTION_MISC_STATEMENT(z, n, nameStem) \
 	if(POET_ACTIVE_FUNCTION_ARG_NAME(~, n, nameStem).has_exception() == true) return true;
-			BOOST_PP_REPEAT(POET_ACTIVE_FUNCTION_NUM_ARGS, POET_ACTIVE_FUNCTION_MISC_STATEMENT, _arg)
+				BOOST_PP_REPEAT(POET_ACTIVE_FUNCTION_NUM_ARGS, POET_ACTIVE_FUNCTION_MISC_STATEMENT, _arg)
 #undef POET_ACTIVE_FUNCTION_MISC_STATEMENT
-			/* We also return ready when all the future inputs are ready, and the guard is true. */
+				/* We also return ready when all the future inputs are ready, and the guard is true. */
 // if(_argn.ready() == false) return false;
 #define POET_ACTIVE_FUNCTION_MISC_STATEMENT(z, n, nameStem) \
 	if(POET_ACTIVE_FUNCTION_ARG_NAME(~, n, nameStem).ready() == false) return false;
-			BOOST_PP_REPEAT(POET_ACTIVE_FUNCTION_NUM_ARGS, POET_ACTIVE_FUNCTION_MISC_STATEMENT, _arg)
+// if(_arg1.ready() == false) return false;
+// if(_arg2.ready() == false) return false;
+// ...
+// if(_argN.ready() == false) return false;
+				BOOST_PP_REPEAT(POET_ACTIVE_FUNCTION_NUM_ARGS, POET_ACTIVE_FUNCTION_MISC_STATEMENT, _arg)
 #undef POET_ACTIVE_FUNCTION_MISC_STATEMENT
-			if(_guard) return (*_guard)();
-			return true;
-		}
-	protected:
-		POET_AF_METHOD_REQUEST_CLASS_NAME(const promise<passive_result_type> &returnValue,
-			POET_ACTIVE_FUNCTION_FULL_ARGS(POET_ACTIVE_FUNCTION_NUM_ARGS, Signature) BOOST_PP_COMMA_IF(POET_ACTIVE_FUNCTION_NUM_ARGS)
-			const boost::shared_ptr<boost::slot<Signature> > &passive_function,
-			const boost::shared_ptr<boost::slot<bool ()> > &guard): base_type(returnValue),
-			BOOST_PP_ENUM(POET_ACTIVE_FUNCTION_NUM_ARGS, POET_ACTIVE_FUNCTION_ARG_CONSTRUCTOR, ~) BOOST_PP_COMMA_IF(POET_ACTIVE_FUNCTION_NUM_ARGS)
-			_passive_function(passive_function), _guard(guard)
-		{
-			_lastReadyChanged = ready();
-		}
-		virtual void postconstruct()
-		{
-			base_type::postconstruct();
-			typedef typename boost::slot<void (void)> slot_type;
-			/*
-			_arg1.connectUpdate(slot_type(
-				&POET_AF_METHOD_REQUEST_CLASS_NAME<Signature>::futureUpdateSlot, this).track(this->shared_from_this()));
-			_arg2.connectUpdate(slot_type(
-				&POET_AF_METHOD_REQUEST_CLASS_NAME<Signature>::futureUpdateSlot, this).track(this->shared_from_this()));
-			...
-			_argN.connectUpdate(slot_type(
-				&POET_AF_METHOD_REQUEST_CLASS_NAME<Signature>::futureUpdateSlot, this).track(this->shared_from_this()));
-			*/
+				if(_guard) return (*_guard)();
+				return true;
+			}
+		protected:
+			POET_AF_METHOD_REQUEST_CLASS_NAME(const promise<passive_result_type> &returnValue,
+				POET_ACTIVE_FUNCTION_FULL_ARGS(POET_ACTIVE_FUNCTION_NUM_ARGS, Signature) BOOST_PP_COMMA_IF(POET_ACTIVE_FUNCTION_NUM_ARGS)
+				const boost::shared_ptr<boost::slot<Signature> > &passive_function,
+				const boost::shared_ptr<boost::slot<bool ()> > &guard): base_type(returnValue),
+				BOOST_PP_ENUM(POET_ACTIVE_FUNCTION_NUM_ARGS, POET_ACTIVE_FUNCTION_ARG_CONSTRUCTOR, ~) BOOST_PP_COMMA_IF(POET_ACTIVE_FUNCTION_NUM_ARGS)
+				_passive_function(passive_function), _guard(guard)
+			{
+				_lastReadyChanged = ready();
+			}
+			virtual void postconstruct()
+			{
+				base_type::postconstruct();
+				typedef typename boost::slot<void (void)> slot_type;
+				/*
+				_arg1.connectUpdate(slot_type(
+					&POET_AF_METHOD_REQUEST_CLASS_NAME<Signature>::futureUpdateSlot, this).track(this->shared_from_this()));
+				_arg2.connectUpdate(slot_type(
+					&POET_AF_METHOD_REQUEST_CLASS_NAME<Signature>::futureUpdateSlot, this).track(this->shared_from_this()));
+				...
+				_argN.connectUpdate(slot_type(
+					&POET_AF_METHOD_REQUEST_CLASS_NAME<Signature>::futureUpdateSlot, this).track(this->shared_from_this()));
+				*/
 #define POET_ACTIVE_FUNCTION_MISC_STATEMENT(z, n, nameStem) \
 	POET_ACTIVE_FUNCTION_ARG_NAME(~, n, nameStem).connect_update(slot_type( \
 		&POET_AF_METHOD_REQUEST_CLASS_NAME<Signature>::futureUpdateSlot, this).track(this->shared_from_this()));
 				BOOST_PP_REPEAT(POET_ACTIVE_FUNCTION_NUM_ARGS, POET_ACTIVE_FUNCTION_MISC_STATEMENT, _arg)
 #undef POET_ACTIVE_FUNCTION_MISC_STATEMENT
-		}
-	private:
-		bool futureArgIsCancelled() const
-		{
+			}
+		private:
+			bool futureArgIsCancelled() const
+			{
 // if(_argn.cancelled()) return true;
 #define POET_ACTIVE_FUNCTION_MISC_STATEMENT(z, n, nameStem) \
 	if(POET_ACTIVE_FUNCTION_ARG_NAME(~, n, nameStem).has_exception()) return true;
+// if(_arg1.cancelled()) return true;
+// if(_arg2.cancelled()) return true;
+// ...
+// if(_argN.cancelled()) return true;
 				BOOST_PP_REPEAT(POET_ACTIVE_FUNCTION_NUM_ARGS, POET_ACTIVE_FUNCTION_MISC_STATEMENT, _arg)
 #undef POET_ACTIVE_FUNCTION_MISC_STATEMENT
-			return false;
-		}
-		void futureUpdateSlot()
-		{
-			boost::mutex::scoped_lock lock(_lastReadyChangedMutex);
-			bool newReady = ready();
-			if(newReady != _lastReadyChanged)
-			{
-				_lastReadyChanged = newReady;
-				this->_updateSignal();
+				return false;
 			}
-			if(futureArgIsCancelled())
+			void futureUpdateSlot()
 			{
-				this->cancel();
-				/* method_request<>::cancel() emits method_request_base::_updateSignal,
-				and cancels future return value, so there is no need to do it here. */
+				boost::mutex::scoped_lock lock(_lastReadyChangedMutex);
+				bool newReady = ready();
+				if(newReady != _lastReadyChanged)
+				{
+					_lastReadyChanged = newReady;
+					this->_updateSignal();
+				}
+				if(futureArgIsCancelled())
+				{
+					this->cancel();
+					/* method_request<>::cancel() emits method_request_base::_updateSignal,
+					and cancels future return value, so there is no need to do it here. */
+				}
+				/* We don't need to worry about cancellation through the return value here,
+				as that is already handled by method_request<> base class. */
+			};
+			void m_run(void *)
+			{
+				(*_passive_function)(
+					POET_ACTIVE_FUNCTION_ARG_NAMES(POET_ACTIVE_FUNCTION_NUM_ARGS, _arg));
+				this->_returnValue.fulfill();
 			}
-			/* We don't need to worry about cancellation through the return value here,
-			as that is already handled by method_request<> base class. */
+			template <typename U>
+			void m_run(U *)
+			{
+				this->_returnValue.fulfill((*_passive_function)(
+					POET_ACTIVE_FUNCTION_ARG_NAMES(POET_ACTIVE_FUNCTION_NUM_ARGS, _arg)));
+			}
+
+			// typename poet::future<boost::function_traits<Signature>::arg1_type> _arg1;
+			// typename poet::future<boost::function_traits<Signature>::arg2_type> _arg2;
+			// ...
+			// typename poet::future<boost::function_traits<Signature>::argN_type> _argN;
+			BOOST_PP_REPEAT(POET_ACTIVE_FUNCTION_NUM_ARGS, POET_ACTIVE_FUNCTION_ARG_DECLARATION, Signature)
+			boost::shared_ptr<boost::slot<Signature> > _passive_function;
+			boost::shared_ptr<boost::slot<bool ()> > _guard;
+			bool _lastReadyChanged;
+			mutable boost::mutex _lastReadyChangedMutex;
 		};
-		void m_run(void *)
-		{
-			(*_passive_function)(
-				POET_ACTIVE_FUNCTION_ARG_NAMES(POET_ACTIVE_FUNCTION_NUM_ARGS, _arg));
-			this->_returnValue.fulfill();
-		}
-		template <typename U>
-		void m_run(U *)
-		{
-			this->_returnValue.fulfill((*_passive_function)(
-				POET_ACTIVE_FUNCTION_ARG_NAMES(POET_ACTIVE_FUNCTION_NUM_ARGS, _arg)));
-		}
 
-		// typename poet::future<boost::function_traits<Signature>::arg1_type> _arg1;
-		// typename poet::future<boost::function_traits<Signature>::arg2_type> _arg2;
-		// ...
-		// typename poet::future<boost::function_traits<Signature>::argN_type> _argN;
-		BOOST_PP_REPEAT(POET_ACTIVE_FUNCTION_NUM_ARGS, POET_ACTIVE_FUNCTION_ARG_DECLARATION, Signature)
-		boost::shared_ptr<boost::slot<Signature> > _passive_function;
-		boost::shared_ptr<boost::slot<bool ()> > _guard;
-		bool _lastReadyChanged;
-		mutable boost::mutex _lastReadyChangedMutex;
-	};
-
-	namespace detail
-	{
 		template<typename Signature>
 		class POET_ACTIVE_FUNCTION_CLASS_NAME
 		{
