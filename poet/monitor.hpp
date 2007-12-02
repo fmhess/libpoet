528e471b101519744908b8a300b8246443d788be
Frank Mori Hess <fmhess@speakeasy.net>
2007-12-02 02:19:16 +0000

    Split off poet::try_monitor and poet::timed_monitor variants off from poet::monitor.

--- poet/monitor.hpp
+++ poet/monitor.hpp
@@ -18,82 +18,154 @@
 
 namespace poet
 {
-	template<typename T, typename Mutex = boost::timed_mutex>
-	class monitor
+	namespace detail
 	{
-	public:
-		typedef T value_type;
-		typedef Mutex mutex_type;
-
-		class scoped_lock: public detail::monitor_scoped_lock<T, Mutex>
+		template<typename MonitorPtr, typename T, typename Mutex>
+		class try_monitor_template;
+		template<typename MonitorPtr, typename T, typename Mutex>
+		class timed_monitor_template;
+		
+		template<typename MonitorPtr, typename T, typename Mutex>
+		class monitor_template
 		{
-			typedef typename detail::monitor_scoped_lock<T, Mutex> base_class;
 		public:
-			scoped_lock(monitor<T, Mutex> &mon): base_class(mon._monitor_pointer)
-			{}
-			scoped_lock(monitor<T, Mutex> &mon, bool do_lock): base_class(mon._monitor_pointer, do_lock)
-			{}
-		};
-		class scoped_try_lock: public detail::monitor_scoped_try_lock<T, Mutex>
-		{
-			typedef typename detail::monitor_scoped_try_lock<T, Mutex> base_class;
-		public:
-			scoped_try_lock(monitor<T, Mutex> &mon): base_class(mon._monitor_pointer)
-			{}
-			scoped_try_lock(monitor<T, Mutex> &mon, bool do_lock): base_class(mon._monitor_pointer, do_lock)
-			{}
-		};
-		class scoped_timed_lock: public detail::monitor_scoped_timed_lock<T, Mutex>
-		{
-			typedef typename detail::monitor_scoped_timed_lock<T, Mutex> base_class;
-		public:
-			template<typename Timeout>
-			scoped_timed_lock(monitor<T, Mutex> &mon, const Timeout &t): base_class(mon._monitor_pointer, t)
-			{}
-			scoped_timed_lock(monitor<T, Mutex> &mon, bool do_lock): base_class(mon._monitor_pointer, do_lock)
-			{}
-		};
+			typedef T value_type;
+			typedef Mutex mutex_type;
 
-		monitor()
-		{}
-		monitor(const T &object): _monitor_pointer(new T(object))
-		{}
-		template<typename U, typename M>
-		monitor(monitor<U, M> &other)
-		{
-			typename monitor<U, M>::scoped_lock lock(other);
-			_monitor_pointer.reset(new T((*lock)));
-		}
+			class scoped_lock: public detail::monitor_scoped_lock<T, Mutex>
+			{
+				typedef typename detail::monitor_scoped_lock<T, Mutex> base_class;
+			public:
+				scoped_lock(monitor_template<MonitorPtr, T, Mutex> &mon): base_class(mon._monitor_pointer)
+				{}
+				scoped_lock(monitor_template<MonitorPtr, T, Mutex> &mon, bool do_lock):
+					base_class(mon._monitor_pointer, do_lock)
+				{}
+			};
+			
+			monitor_template(const T &object): _monitor_pointer(new T(object))
+			{}
+			template<typename OtherMonitorPtr, typename U, typename M>
+			monitor_template(monitor_template<OtherMonitorPtr, U, M> &other)
+			{
+				typename monitor_template<OtherMonitorPtr, U, M>::scoped_lock lock(other);
+				_monitor_pointer.reset(new T((*lock)));
+			}
 
-		monitor& operator=(const T &rhs)
-		{
-			if(_monitor_pointer)
+			monitor_template& operator=(const T &rhs)
 			{
 				scoped_lock lock(*this);
 				*lock = rhs;
-			}else
-			{
-				_monitor_pointer.reset(new T(rhs));
+				return *this;
 			}
-			return *this;
-		}
-		template<typename U, typename M>
-		monitor& operator=(monitor<U, M> &rhs)
-		{
-			if(&rhs == this) return *this;
+			template<typename OtherMonitorPtr, typename U, typename M>
+			monitor_template& operator=(monitor_template<OtherMonitorPtr, U, M> &rhs)
+			{
+				if(&rhs == this) return *this;
 
-			boost::scoped_ptr<T> temp;
-			/* Avoid locking the mutexes of both this monitor and the
-			other monitor simultaneously, since we don't want to invite
-			any potential locking order violations. */
-			{
-				typename monitor<U, M>::scoped_lock other_lock(rhs);
-				temp.reset(new T(*other_lock));
+				boost::scoped_ptr<T> temp;
+				/* Avoid locking the mutexes of both this monitor and the
+				other monitor simultaneously, since we don't want to invite
+				any potential locking order violations. */
+				{
+					typename monitor_template<OtherMonitorPtr, U, M>::scoped_lock other_lock(rhs);
+					temp.reset(new T(*other_lock));
+				}
+				return *this = *temp;
 			}
-			return *this = *temp;
-		}
+		protected:
+			friend class try_monitor_template<MonitorPtr, T, Mutex>;
+			friend class timed_monitor_template<MonitorPtr, T, Mutex>;
+			
+			MonitorPtr _monitor_pointer;
+		};
+		
+		template<typename MonitorPtr, typename T, typename Mutex>
+		class try_monitor_template: public monitor_template<MonitorPtr, T, Mutex>
+		{
+			typedef monitor_template<MonitorPtr, T, Mutex> base_class;
+		public:
+			class scoped_try_lock: public detail::monitor_scoped_try_lock<T, Mutex>
+			{
+				typedef typename detail::monitor_scoped_try_lock<T, Mutex> base_class;
+			public:
+				scoped_try_lock(monitor_template<MonitorPtr, T, Mutex> &mon): base_class(mon._monitor_pointer)
+				{}
+				scoped_try_lock(monitor_template<MonitorPtr, T, Mutex> &mon, bool do_lock):
+					base_class(mon._monitor_pointer, do_lock)
+				{}
+			};
+			
+			try_monitor_template(const T &object): base_class(object)
+			{}
+			template<typename OtherMonitorPtr, typename U, typename M>
+			try_monitor_template(monitor_template<OtherMonitorPtr, U, M> &other): base_class(other)
+			{}
+		};
+	
+		template<typename MonitorPtr, typename T, typename Mutex>
+		class timed_monitor_template: public try_monitor_template<MonitorPtr, T, Mutex>
+		{
+			typedef try_monitor_template<MonitorPtr, T, Mutex> base_class;
+		public:
+			class scoped_timed_lock: public detail::monitor_scoped_timed_lock<T, Mutex>
+			{
+				typedef typename detail::monitor_scoped_timed_lock<T, Mutex> base_class;
+			public:
+				template<typename Timeout>
+				scoped_timed_lock(monitor_template<MonitorPtr, T, Mutex> &mon, const Timeout &t):
+					base_class(mon._monitor_pointer, t)
+				{}
+				scoped_timed_lock(monitor_template<MonitorPtr, T, Mutex> &mon, bool do_lock):
+					base_class(mon._monitor_pointer, do_lock)
+				{}
+			};
+			
+			timed_monitor_template(const T &object): base_class(object)
+			{}
+			template<typename OtherMonitorPtr, typename U, typename M>
+			timed_monitor_template(monitor_template<OtherMonitorPtr, U, M> &other): base_class(other)
+			{}
+		};
+	};
+
+	template<typename T, typename Mutex = boost::mutex>
+	class monitor: public detail::monitor_template<monitor_ptr<T, Mutex>, T, Mutex>
+	{
 	private:
-		timed_monitor_ptr<T, Mutex> _monitor_pointer;
+		typedef typename detail::monitor_template<monitor_ptr<T, Mutex>, T, Mutex> base_class;
+	public:
+		monitor(const T &object): base_class(object)
+		{}
+		template<typename U, typename M>
+		monitor(monitor<U, M> &other): base_class(other)
+		{}
+	};
+	
+	template<typename T, typename Mutex = boost::try_mutex>
+	class try_monitor: public detail::try_monitor_template<try_monitor_ptr<T, Mutex>, T, Mutex>
+	{
+	private:
+		typedef typename detail::try_monitor_template<try_monitor_ptr<T, Mutex>, T, Mutex> base_class;
+	public:
+		try_monitor(const T &object): base_class(object)
+		{}
+		template<typename U, typename M>
+		try_monitor(try_monitor<U, M> &other): base_class(other)
+		{}
+	};
+	
+	template<typename T, typename Mutex = boost::timed_mutex>
+	class timed_monitor: public detail::timed_monitor_template<timed_monitor_ptr<T, Mutex>, T, Mutex>
+	{
+	private:
+		typedef typename detail::timed_monitor_template<timed_monitor_ptr<T, Mutex>, T, Mutex> base_class;
+	public:
+		timed_monitor(const T &object): base_class(object)
+		{}
+		template<typename U, typename M>
+		timed_monitor(try_monitor<U, M> &other): base_class(other)
+		{}
 	};
 };
 
