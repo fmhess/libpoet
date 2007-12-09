19bc81572535b963127dff9b860606c149ba6927
Frank Mori Hess <fmhess@speakeasy.net>
2007-12-09 21:52:28 +0000

    Collapsed monitor, try_monitor, and timed_monitor into a single monitor class with the magic of new mutex_properties<> class, and partial template specialization.

--- poet/monitor.hpp
+++ poet/monitor.hpp
@@ -14,116 +14,133 @@
 
 #include <boost/scoped_ptr.hpp>
 #include <poet/detail/monitor_locks.hpp>
+#include <poet/mutex_properties.hpp>
 #include <poet/monitor_ptr.hpp>
 
 namespace poet
 {
-	template<typename T, typename Mutex>
-	class try_monitor;
-	template<typename T, typename Mutex>
-	class timed_monitor;
+	namespace detail
+	{
+		template<typename T, typename Mutex, enum mutex_model>
+		class specialized_monitor
+		{};
+		
+		template<typename T, typename Mutex>
+		class specialized_monitor<T, Mutex, mutex_concept>
+		{
+		public:
+			typedef T value_type;
+			typedef Mutex mutex_type;
+
+			class scoped_lock: public detail::monitor_scoped_lock<T, Mutex>
+			{
+				typedef typename detail::monitor_scoped_lock<T, Mutex> base_class;
+			public:
+				scoped_lock(specialized_monitor<T, Mutex, mutex_concept> &mon): base_class(mon._monitor_pointer)
+				{}
+				scoped_lock(specialized_monitor<T, Mutex, mutex_concept> &mon, bool do_lock):
+					base_class(mon._monitor_pointer, do_lock)
+				{}
+			};
+
+			specialized_monitor(const T &object): _monitor_pointer(new T(object))
+			{}
+			template<typename U, typename M>
+			specialized_monitor(specialized_monitor<U, M, mutex_concept> &other)
+			{
+				typename specialized_monitor<U, M, mutex_concept>::scoped_lock lock(other);
+				_monitor_pointer.reset(new T((*lock)));
+			}
+			virtual ~specialized_monitor() {}
+
+			specialized_monitor& operator=(const T &rhs)
+			{
+				scoped_lock lock(*this);
+				*lock = rhs;
+				return *this;
+			}
+			template<typename U, typename M>
+			specialized_monitor& operator=(specialized_monitor<U, M, mutex_concept> &rhs)
+			{
+				if(&rhs == this) return *this;
+
+				boost::scoped_ptr<T> temp;
+				/* Avoid locking the mutexes of both this monitor and the
+				other monitor simultaneously, since we don't want to invite
+				any potential locking order violations. */
+				{
+					typename specialized_monitor<U, M, mutex_concept>::scoped_lock other_lock(rhs);
+					temp.reset(new T(*other_lock));
+				}
+				return *this = *temp;
+			}
+		protected:
+			template<typename U, typename M, enum mutex_model model>
+			friend class specialized_monitor;
+
+			monitor_ptr<T, Mutex> _monitor_pointer;
+		};
+
+		template<typename T, typename Mutex>
+		class specialized_monitor<T, Mutex, try_mutex_concept>:
+			public specialized_monitor<T, Mutex, mutex_concept>
+		{
+			typedef specialized_monitor<T, Mutex, mutex_concept> base_class;
+		public:
+			class scoped_try_lock: public detail::monitor_scoped_try_lock<T, Mutex>
+			{
+				typedef typename detail::monitor_scoped_try_lock<T, Mutex> base_class;
+			public:
+				scoped_try_lock(specialized_monitor<T, Mutex, try_mutex_concept> &mon): base_class(mon._monitor_pointer)
+				{}
+				scoped_try_lock(specialized_monitor<T, Mutex, try_mutex_concept> &mon, bool do_lock):
+					base_class(mon._monitor_pointer, do_lock)
+				{}
+			};
+
+			specialized_monitor(const T &object): base_class(object)
+			{}
+			template<typename U, typename M, enum mutex_model model>
+			specialized_monitor(specialized_monitor<U, M, model> &other): base_class(other)
+			{}
+		};
+
+		template<typename T, typename Mutex>
+		class specialized_monitor<T, Mutex, timed_mutex_concept>:
+			public specialized_monitor<T, Mutex, try_mutex_concept>
+		{
+			typedef specialized_monitor<T, Mutex, try_mutex_concept> base_class;
+		public:
+			class scoped_timed_lock: public detail::monitor_scoped_timed_lock<T, Mutex>
+			{
+				typedef typename detail::monitor_scoped_timed_lock<T, Mutex> base_class;
+			public:
+				template<typename Timeout>
+				scoped_timed_lock(specialized_monitor<T, Mutex, timed_mutex_concept> &mon, const Timeout &t):
+					base_class(mon._monitor_pointer, t)
+				{}
+				scoped_timed_lock(specialized_monitor<T, Mutex, timed_mutex_concept> &mon, bool do_lock):
+					base_class(mon._monitor_pointer, do_lock)
+				{}
+			};
+
+			specialized_monitor(const T &object): base_class(object)
+			{}
+			template<typename U, typename M, enum mutex_model model>
+			specialized_monitor(specialized_monitor<U, M, model> &other): base_class(other)
+			{}
+		};
+	};
 
 	template<typename T, typename Mutex = boost::mutex>
-	class monitor
+	class monitor: public detail::specialized_monitor<T, Mutex, mutex_properties<Mutex>::model>
 	{
+		typedef typename detail::specialized_monitor<T, Mutex, mutex_properties<Mutex>::model> base_class;
 	public:
-		typedef T value_type;
-		typedef Mutex mutex_type;
-
-		class scoped_lock: public detail::monitor_scoped_lock<T, Mutex>
-		{
-			typedef typename detail::monitor_scoped_lock<T, Mutex> base_class;
-		public:
-			scoped_lock(monitor<T, Mutex> &mon): base_class(mon._monitor_pointer)
-			{}
-			scoped_lock(monitor<T, Mutex> &mon, bool do_lock):
-				base_class(mon._monitor_pointer, do_lock)
-			{}
-		};
-
-		monitor(const T &object): _monitor_pointer(new T(object))
+		monitor(const T &object): base_class(object)
 		{}
 		template<typename U, typename M>
-		monitor(monitor<U, M> &other)
-		{
-			typename monitor<U, M>::scoped_lock lock(other);
-			_monitor_pointer.reset(new T((*lock)));
-		}
-		virtual ~monitor() {}
-
-		monitor& operator=(const T &rhs)
-		{
-			scoped_lock lock(*this);
-			*lock = rhs;
-			return *this;
-		}
-		template<typename U, typename M>
-		monitor& operator=(monitor<U, M> &rhs)
-		{
-			if(&rhs == this) return *this;
-
-			boost::scoped_ptr<T> temp;
-			/* Avoid locking the mutexes of both this monitor and the
-			other monitor simultaneously, since we don't want to invite
-			any potential locking order violations. */
-			{
-				typename monitor<U, M>::scoped_lock other_lock(rhs);
-				temp.reset(new T(*other_lock));
-			}
-			return *this = *temp;
-		}
-	protected:
-		friend class try_monitor<T, Mutex>;
-		friend class timed_monitor<T, Mutex>;
-
-		monitor_ptr<T, Mutex> _monitor_pointer;
-	};
-
-	template<typename T, typename Mutex = boost::try_mutex>
-	class try_monitor: public monitor<T, Mutex>
-	{
-		typedef monitor<T, Mutex> base_class;
-	public:
-		class scoped_try_lock: public detail::monitor_scoped_try_lock<T, Mutex>
-		{
-			typedef typename detail::monitor_scoped_try_lock<T, Mutex> base_class;
-		public:
-			scoped_try_lock(try_monitor<T, Mutex> &mon): base_class(mon._monitor_pointer)
-			{}
-			scoped_try_lock(try_monitor<T, Mutex> &mon, bool do_lock):
-				base_class(mon._monitor_pointer, do_lock)
-			{}
-		};
-
-		try_monitor(const T &object): base_class(object)
-		{}
-		template<typename U, typename M>
-		try_monitor(monitor<U, M> &other): base_class(other)
-		{}
-	};
-
-	template<typename T, typename Mutex = boost::timed_mutex>
-	class timed_monitor: public try_monitor<T, Mutex>
-	{
-		typedef try_monitor<T, Mutex> base_class;
-	public:
-		class scoped_timed_lock: public detail::monitor_scoped_timed_lock<T, Mutex>
-		{
-			typedef typename detail::monitor_scoped_timed_lock<T, Mutex> base_class;
-		public:
-			template<typename Timeout>
-			scoped_timed_lock(timed_monitor<T, Mutex> &mon, const Timeout &t):
-				base_class(mon._monitor_pointer, t)
-			{}
-			scoped_timed_lock(timed_monitor<T, Mutex> &mon, bool do_lock):
-				base_class(mon._monitor_pointer, do_lock)
-			{}
-		};
-
-		timed_monitor(const T &object): base_class(object)
-		{}
-		template<typename U, typename M>
-		timed_monitor(monitor<U, M> &other): base_class(other)
+		monitor(monitor<U, M> &other): base_class(other)
 		{}
 	};
 };
---
+++ poet/mutex_properties.hpp
@@ -0,0 +1,106 @@
+/*
+	A template class for getting properties of a mutex.  Covers mutex types from
+	boost.thread and libpoet.  Other mutex classes can provide their own
+	specializations.
+
+	begin: Frank Hess <fmhess@users.sourceforge.net>  2007-12-07
+*/
+
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef _POET_MUTEX_PROPERTIES_HPP
+#define _POET_MUTEX_PROPERTIES_HPP
+
+namespace boost
+{
+	class mutex;
+	class try_mutex;
+	class timed_mutex;
+	class recursive_mutex;
+	class recursive_try_mutex;
+	class recursive_timed_mutex;
+};
+
+namespace poet
+{
+	template<typename Mutex, typename Key, typename KeyCompare> class acyclic_mutex;
+	template<typename T, typename Mutex> class monitor;
+	template<typename T, typename Mutex> class monitor_ptr;
+	
+	enum mutex_model
+	{
+		mutex_concept,
+		try_mutex_concept,
+		timed_mutex_concept
+	};
+
+	template<typename Mutex> class mutex_properties
+	{};
+	
+	template<>
+	class mutex_properties<boost::mutex>
+	{
+	public:
+		static const bool recursive = false;
+		static const mutex_model model = mutex_concept;
+	};
+	
+	template<>
+	class mutex_properties<boost::try_mutex>
+	{
+	public:
+		static const bool recursive = false;
+		static const mutex_model model = try_mutex_concept;
+	};
+	
+	template<>
+	class mutex_properties<boost::timed_mutex>
+	{
+	public:
+		static const bool recursive = false;
+		static const mutex_model model = timed_mutex_concept;
+	};
+
+	template<>
+	class mutex_properties<boost::recursive_mutex>
+	{
+	public:
+		static const bool recursive = true;
+		static const mutex_model model = mutex_concept;
+	};
+	
+	template<>
+	class mutex_properties<boost::recursive_try_mutex>
+	{
+	public:
+		static const bool recursive = true;
+		static const mutex_model model = try_mutex_concept;
+	};
+	
+	template<>
+	class mutex_properties<boost::recursive_timed_mutex>
+	{
+	public:
+		static const bool recursive = true;
+		static const mutex_model model = timed_mutex_concept;
+	};
+	
+	template<typename Mutex, typename Key, typename KeyCompare>
+	class mutex_properties<poet::acyclic_mutex<Mutex, Key, KeyCompare> >:
+		public mutex_properties<Mutex>
+	{};
+
+	template<typename T, typename Mutex>
+	class mutex_properties<poet::monitor<T, Mutex> >:
+		public mutex_properties<Mutex>
+	{};
+
+	template<typename T, typename Mutex>
+	class mutex_properties<poet::monitor_ptr<T, Mutex> >:
+		public mutex_properties<Mutex>
+	{};
+};
+
+#endif // _POET_MUTEX_PROPERTIES_HPP
